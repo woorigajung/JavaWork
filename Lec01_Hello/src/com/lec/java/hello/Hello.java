4203bf078c25a8a7237f4c5409a2de3e8b4b35aa
woorigajung <woorigajung@naver.com>
2020-05-06 17:23:11 +0900

    2020-05-06 반응형 웹

--- Lec01_Hello/src/com/lec/java/hello/Hello.java
+++ Lec01_Hello/src/com/lec/java/hello/Hello.java
@@ -1,51 +1,58 @@
 package com.lec.java.hello;
 
-//	초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+import java.util.TreeMap;
+
+//	문제 설명
+//	선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
 //	
-//	제한사항
-//	prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
-//	prices의 길이는 2 이상 100,000 이하입니다.
+//	예를 들어 선행 스킬 순서가 스파크 → 라이트닝 볼트 → 썬더일때, 썬더를 배우려면 먼저 라이트닝 볼트를 배워야 하고, 
+//	라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다.
+//	
+//	위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 스파크 → 힐링 → 라이트닝 볼트 → 썬더와 같은 스킬트리는 가능하지만
+//	, 썬더 → 스파크나 라이트닝 볼트 → 스파크 → 힐링 → 썬더와 같은 스킬트리는 불가능합니다.
+//	
+//	선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질 때,
+//	가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+//	
+//	제한 조건
+//	스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
+//	스킬 순서와 스킬트리는 문자열로 표기합니다.
+//	예를 들어, C → B → D 라면 CBD로 표기합니다
+//	선행 스킬 순서 skill의 길이는 1 이상 26 이하이며, 스킬은 중복해 주어지지 않습니다.
+//	skill_trees는 길이 1 이상 20 이하인 배열입니다.
+//	skill_trees의 원소는 스킬을 나타내는 문자열입니다.
+//	skill_trees의 원소는 길이가 2 이상 26 이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
 //	입출력 예
-//	prices	return
-//	[1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]
-//	입출력 예 설명
-//	1초 시점의 ₩1은 끝까지 가격이 떨어지지 않았습니다.
-//	2초 시점의 ₩2은 끝까지 가격이 떨어지지 않았습니다.
-//	3초 시점의 ₩3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
-//	4초 시점의 ₩2은 1초간 가격이 떨어지지 않았습니다.
-//	5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
+//	skill	skill_trees	return
+//	"CBD"	["BACDE", "CBADF", "AECB", "BDA"]	2
 public class Hello {
 
 	public static void main(String[] args) {
+		Hello a = new Hello();
 
-		Hello i = new Hello();
-		while(true) {
-			System.out.println(i.solution(new int[] { 1, 2, 3, 2, 3 })[i]);
-		}
 		
-
 	} // end main()
 	// https://www.youtube.com/watch?v=S6YFtFJma4Q
 	// https://www.youtube.com/watch?v=Ldb6ixbKoUU
+	
+	    public int solution(String skill, String[] skill_trees) {
+	    	TreeMap<String, Integer> skillTree = new TreeMap<String, Integer>();
+	    	
+	    	for (int i = 0; i < skill_trees.length; i++) {
+				skillTree.put(skill_trees[i], i);
+			}
+	    	
+	    	for (int i = 0; i < skill.length(); i++) {
+	    		skillTree.get(skill.charAt(i));
+	    		
+				
+			}
 
-	public int[] solution(int[] prices) {
-		int[] answer = new int[prices.length];
-		int cnt = 0;
-		for (int i = 0; i < prices.length; i++) {
-			cnt = 0;
-			for (int j = i + 1; j < answer.length; j++) {
-				if (prices[j] >= prices[i]) {
-					cnt++;
-				}else {
-					break;
-				} // end if
-			} // end for
-			
-			answer[i] = cnt;
-			
-		} // end for
-		
-		return answer;
-	} // end solution
+	        
+	        return answer;
+    } // end solution
+
+
+	
 
 } // end class
